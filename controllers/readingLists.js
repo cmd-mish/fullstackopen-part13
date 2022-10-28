e6993a90e742b221b99678eae694ada6840128d1
cmd-mish <popov.mikhail.al@gmail.com>
2022-10-28 19:17:40 +0300

    Exercise 13.22

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -14,4 +14,18 @@ router.post('/', tokenExtractor, async (req, res) => {
   res.send(body)
 })
 
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const readingList = await ReadingList.findByPk(req.params.id)
+
+  if (readingList) {
+    if (req.decodedToken.id !== readingList.userId) return res.status(401).json({ error: 'you don not have permission to change this reading list' })
+
+    readingList.read = req.body.read
+    await readingList.save()
+    return res.json(readingList)
+  }
+
+  return res.status(404).json({ error: 'list with this id was not found' })
+})
+
 module.exports = router
